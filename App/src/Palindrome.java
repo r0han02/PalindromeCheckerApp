23df0e4abb8b78f4b8d3e95057a4d7ac120ac2a0
Rohan <rp1763@srmist.edu.in>
2026-03-02 09:04:02 +0530

    uc1 submitted

---
+++ App/src/Palindrome.java
@@ -0,0 +1,7 @@
+public class Palindrome {
+    static void main() {
+        System.out.println("Welcome to the Palindrome Checker Management System");
+        System.out.println("Version : 1.0");
+        System.out.println("System initialized   successfully.");;
+    }
+}
